136aafe718f94e5f32bac12fbfccc936de39ee00
luca044 <158043350+luca044@users.noreply.github.com>
2024-02-02 12:43:57 +0000

    .4

--- ik.c
+++ ik.c
@@ -1,6 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+void quiz(){
+    sleep(2);
+                    system("clear");
+                    printf(" Vuoi ricevere un buono sconto per l'acquisto partecipando ad un quiz?\n");
+                    int q;
+                    printf(" 1. Si, partecipo\n");
+                    printf(" 2. No, non partecipo\n");
+                    scanf(" %d", &q);
+                    
+                    system("clear");
+                    if (q==1)
+                    {
+                        int y=0;
+                    int t;
+                    printf("In quale città ha sede il festival della musica dance ed elettronica “Tomorrowland” ?\n\n1. Boom, in Belgio\n2. Parigi, in Francia\n3. Amsterdam, in Olanda\n4. Madrid, in Spagna\n");
+                    
+                    
+                    scanf(" %d", &t);
+                    if(t==1){
+                        y=y+1;
+                        printf(" CORRETTO     punteggio: %d\n", y);
+                    }else{
+                        printf(" SBAGLIATO    punteggio: %d\n", y);
+                    }
+                    sleep(1);
+                    system("clear");
+                    printf("Nel moderno sistema musicale, quante sono, in tutto, le tonalità maggiori e minori?\n\n1.Ventiquattro\n2.Dodici\n3.Venti\n4.Trentasei\n");
+                    
+                    int g;
+                    scanf(" %d", &g);
+                    if(g==1){
+                        y=y+1;
+                        printf(" CORRETTO     punteggio: %d\n", y);
+                    }else{
+                        printf(" SBAGLIATO    punteggio: %d\n", y);
+                    }
+                    sleep(1);
+                    system("clear");
+                    printf("9. In quale opera si canta Nessun dorma?\n\n1.Cavalleria rusticana, di Mascagni\n2.Mosè, di Rossini\n3.Otello, di Verdi\n4.Turandot, di Puccini\n");
+                    
+                    int s;
+                    scanf(" %d", &s);
+                    if(s==4){
+                        y=y+1;
+                        printf(" CORRETTO     punteggio: %d\n", y);
+                    }else{
+                        printf(" SBAGLIATO    punteggio: %d\n", y);
+                    }
+                    sleep(1);
+                    if(y==3){
+                        printf("CONGRATULAZIONI!!!\n\nHai il 5x100 di sconto sull'acquisto del prossimo biglietto\n");
+                    } else{
+                        printf("Peccato, hai perso...\n");
+                    }
+
+                    }
+                    else if (q==2)
+                    {
+                        
+            system("clear");
+            printf("Fine sessione.\n");
+                    }
+                    
+
+}
 
 void questionario()
 {
@@ -39,6 +104,7 @@ void questionario()
                 {
                     system("clear");
                     printf("Procedi all'acquisto del biglietto installando l'app\n");
+                    
                 } 
                 else if(x==4){
                     system("clear");
@@ -83,6 +149,7 @@ void questionario()
                 {
                     system("clear");
                     printf("Procedi all'acquisto del biglietto installando l'app\n");
+                    
                 } 
                 else if(c==4){
                     system("clear");
@@ -125,6 +192,7 @@ void questionario()
                 {
                     system("clear");
                     printf("Procedi all'acquisto del biglietto installando l'app\n");
+                    
                 } 
                 else if(b==4){
                     system("clear");
@@ -168,6 +236,7 @@ void questionario()
                 {
                     system("clear");
                     printf("Procedi all'acquisto del biglietto installando l'app\n");
+                    
                 } 
                 else if(a==4){
                     system("clear");
@@ -210,6 +279,7 @@ void questionario()
                 {
                     system("clear");
                     printf("Procedi all'acquisto del biglietto installando l'app\n");
+                    
                 } 
                 else if(d==4){
                     system("clear");
@@ -283,7 +353,7 @@ int main()
             printf("Inserisci il tuo cognome:\n");
             scanf(" %s", cognome);
             system("clear");
-            printf("Inserisci la regione in cui vivi:\n");
+            printf("Inserisci la città in cui vivi:\n");
             char regione[100];
             scanf(" %s", regione);
             system("clear");
@@ -311,5 +381,7 @@ int main()
         }
     
     }
+    quiz();
+    
     return 0;
 }
